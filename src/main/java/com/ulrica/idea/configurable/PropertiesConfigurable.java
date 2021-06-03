3040a18167742b436a18066fc390a9565803c97a
S9037218 <Orangejuice135!>
2021-06-03 18:37:23 +0800

    modify:减少try块，增加优先取舍

--- src/main/java/com/ulrica/idea/configurable/PropertiesConfigurable.java
+++ src/main/java/com/ulrica/idea/configurable/PropertiesConfigurable.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 
 public class PropertiesConfigurable {
 
-    public static PropertiesDetail readProperties(Project currentProject){
+    public static PropertiesDetail readProperties(Project currentProject) {
         BufferedReader bufferedReader = null;
         try {
             String basePath = currentProject.getBasePath();
@@ -21,27 +21,22 @@ public class PropertiesConfigurable {
             bufferedReader = new BufferedReader(new FileReader(basePath));
             properties.load(bufferedReader);
             PropertiesDetail propertiesDetail = new PropertiesDetail();
-            try {
-                propertiesDetail.setExportDirs(properties.getProperty("exportDirs"));
-                propertiesDetail.setListenDirs(properties.getProperty("listenDirs"));
-                propertiesDetail.setSchedulePushSwitch(convertStringToBoolean(properties.getProperty("schedulePushSwitch")));
-                propertiesDetail.setGitPushSwitch(convertStringToBoolean(properties.getProperty("gitPushSwitch")));
-                propertiesDetail.setFirstTime(properties.getProperty("firstTime"));
-                propertiesDetail.setIntervalTime(properties.getProperty("intervalTime"));
-            }catch (Exception e){
-                e.printStackTrace();
-                Log.info("配置文件读取错误");
-            }
+            propertiesDetail.setExportDirs(properties.getProperty("exportDirs"));
+            propertiesDetail.setListenDirs(properties.getProperty("listenDirs"));
+            propertiesDetail.setSchedulePushSwitch(convertStringToBoolean(properties.getProperty("schedulePushSwitch")));
+            propertiesDetail.setGitPushSwitch(convertStringToBoolean(properties.getProperty("gitPushSwitch")));
+            propertiesDetail.setFirstTime(properties.getProperty("firstTime"));
+            propertiesDetail.setIntervalTime(properties.getProperty("intervalTime"));
             return propertiesDetail;
         } catch (Exception e) {
             e.printStackTrace();
-            Log.info("文件未找到!");
+            Log.info("配置文件读取错误");
             return null;
-        }finally {
-            if(bufferedReader != null){
-                try{
+        } finally {
+            if (bufferedReader != null) {
+                try {
                     bufferedReader.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     Log.info("bufferedReader close IOException:" + e.getMessage());
                 }
             }
@@ -51,10 +46,10 @@ public class PropertiesConfigurable {
 
     private static Boolean convertStringToBoolean(String str) throws Exception {
         Log.info(str);
-        if(str.equals("true")){
+        if (str.equals("true")) {
             return true;
         }
-        if(str.equals("false")){
+        if (str.equals("false")) {
             return false;
         }
         throw new Exception("请正确输入布尔值，true或者false");
--- src/main/java/com/ulrica/idea/extensions/GitPrePushHandler.java
+++ src/main/java/com/ulrica/idea/extensions/GitPrePushHandler.java
@@ -21,15 +21,15 @@ import com.intellij.util.Query;
 import com.intellij.vcs.log.VcsFullCommitDetails;
 import com.ulrica.idea.configurable.PropertiesConfigurable;
 import com.ulrica.idea.configurable.PropertiesDetail;
+import com.ulrica.idea.configurable.SettingConfigurable;
 import com.ulrica.idea.persistent.SettingPersistent;
 import com.ulrica.idea.utils.ProjectUtil;
+import javafx.util.Pair;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -49,18 +49,41 @@ public class GitPrePushHandler implements PrePushHandler {
         return "将变更导出到yapi";
     }
 
+
+    private SettingPersistent getSettingPersistent(Project currentProject) {
+        SettingPersistent instance = SettingPersistent.getInstance(currentProject);
+        if (!instance.gitPushSwitch && !instance.schedulePushSwitch) {
+            LOG.info("idea配置信息为空，将从配置文件中读取！");
+            return null;
+        }
+        LOG.info("检测到idea中存在配置信息，优先采用idea配置，若要关闭，请取消勾选git push与时间任务");
+        return instance;
+    }
+
+    private <T> Pair<String, String> getDirs(T t) {
+        Pair<String, String> pair;
+        if (t.getClass().equals(SettingPersistent.class)) {
+            SettingPersistent settingPersistent = (SettingPersistent) t;
+            pair = new Pair<>(settingPersistent.exportDirs, settingPersistent.listenDirs);
+        } else {
+            PropertiesDetail propertiesDetail = (PropertiesDetail) t;
+            pair = new Pair<>(propertiesDetail.getExportDirs(), propertiesDetail.getListenDirs());
+        }
+        return pair;
+    }
+
     @NotNull
     @Override
     public Result handle(@NotNull List<PushInfo> list, @NotNull ProgressIndicator progressIndicator) {
         Project currentProject = ProjectUtil.getCurrentProject();
-        PropertiesDetail settingPersistent = PropertiesConfigurable.readProperties(currentProject);
-        assert settingPersistent != null;
-        if (!settingPersistent.isGitPushSwitch()) {
-            LOG.info("gitPushSwitch is not open");
-            return Result.OK;
+        Pair<String, String> stringPair;
+        if (getSettingPersistent(currentProject) != null) {
+            stringPair = getDirs(Objects.requireNonNull(getSettingPersistent(currentProject)));
+        } else {
+            stringPair = getDirs(Objects.requireNonNull(PropertiesConfigurable.readProperties(currentProject)));
         }
-        String listenerDir = settingPersistent.getListenDirs();
-        String exportDir = settingPersistent.getExportDirs();
+        String exportDir = stringPair.getKey();
+        String listenerDir = stringPair.getValue();
         if (StringUtils.isAnyBlank(listenerDir, exportDir)) {
             LOG.info("auto api listenDirs or exportDirs not config");
             return Result.OK;
